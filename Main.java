3b39848f50dc41377957f56ead8294ecad2ef08d
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-12 20:46:56 -0700

    initial state

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.fizzBuzz(3));
+        System.out.println(sol.fizzBuzz(5));
+        System.out.println(sol.fizzBuzz(15));
+    }
+}
